57b16c02c93d03882023d3a5c2e1feb22df00e8e
Smart1e <smartieassistant@gmail.com>
2022-05-07 21:13:00 +0100

    Usernames?!?!

--- Data/Setup.py
+++ Data/Setup.py
@@ -1,9 +1,12 @@
 
 
+from pydoc import text
+
+
 try:
     import tkinter as tk
     import tkinter.ttk as ttk
-    
+    import json
 except:
     import os
     from time import sleep
@@ -14,18 +17,39 @@ except:
         
 else:
     from time import sleep
+    import os
     root = tk.Tk()
     audiouse = tk.IntVar()
     micuse = tk.IntVar()
     passuse = 6
     passcode = False
-    def finalize_details():
+    username = tk.StringVar()
+    def finalize_details(file='settings.json'):
         global audiouse
         global micuse
         global passuse
         global passcode
-        print(f'Audio {audiouse.get()}, Mic {micuse.get()}, Passused {passuse}, Passcode {passcode}.')
+        global username
         
+        
+        import json
+        if os.name == 'nt':
+            #Windows
+            system = '2'
+            
+        elif os.name == 'posix':
+            #Mac or Linux
+            system = '1'
+            
+        else:
+            #Unable to find an os name
+            system = '0'
+      
+        settings = [system]
+        myJSON = json.dumps(settings)
+        print(f'Audio {audiouse.get()}, Mic {micuse.get()}, Passused {passuse}, Passcode {passcode}, the system is {system}, username is {username.get()}.')
+        with open(file, "w") as f:
+            f.write(myJSON)
         
     #Themes formatted as [background, text, Window background, border colours]
     theme_dark = ['#1F2140', '#989BCD', '#121426', '#1F2933']
@@ -47,12 +71,18 @@ else:
     
     micro = tk.Checkbutton(root, text = "Would you like microphone to be used by default?", variable = micuse, foreground=theme[1], background=theme[0])
     micro.grid(row=2, column=0, columnspan=4, pady=3)
+
+    ask_user_tell = tk.Entry(root, disabledbackground=theme[0], disabledforeground=theme[1], width=33)
+    ask_user_tell.insert(0, "Please enter a username underneath.")
+    ask_user_tell.config(state='disabled')
+    ask_user_tell.grid(row=3, column=0, columnspan=4, pady=3)
+    
+    ask_user = tk.Entry(root, textvariable=username, foreground=theme[1], background=theme[0])
+    ask_user.grid(row=4, column=0, columnspan=4, pady=3)
     
     enter = tk.Button(root, text='Enter', command=finalize_details, foreground=theme[1], background=theme_accent[0], width=50)
-    enter.grid(row=5, column=0, columnspan=4, pady=3)
+    enter.grid(row=10, column=0, columnspan=4, pady=3)   
     
-    ask_user = tk.Entry
-
-    
+     
     root.mainloop()
     
